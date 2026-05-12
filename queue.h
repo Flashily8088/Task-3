c9af3fee50ebc922b0f473e44986fd1098349cc3
Elizaveta Ryabova <elizaveta.ryabova.04@inbox.ru>
2026-05-12 15:38:57 +0300

    Add files via upload

---
+++ queue.h
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+
+/**
+	* @breaf класс очередь
+	 */
+class queue
+{
+	/**
+	* @breaf размер очереди
+	 */
+	int size; 
+	/**
+	* @breaf индекс первого элемента очереди
+	 */
+	int front; 
+	/**
+	* @breaf индекс последнего элемента очереди
+	 */
+	int back;
+	/**
+	* @breaf массив из элементов очереди
+	 */
+	int* array;
+public:
+	/**
+	* @breaf конструктор по умолчанию
+	 */
+	queue();
+	/**
+	* @breaf конструктор
+	* @param s - размер очереди
+	* @param list - массив из элементов 
+	 */
+	queue(const int s, const int* list);
+	/**
+	* @breaf оператор вывода
+	* @param os - поток вывода
+	* @param Queue - очередь
+	* @return поток вывода
+	 */
+
+	friend ostream& operator <<(ostream& os, const queue Queue);
+	/**
+	* @breaf оператор =
+	* @param Queue - очередь
+	 */
+	queue& operator =(queue& Queue);
+	/**
+	* @breaf вставляет элемент по индексу
+	* @param val - значение элемента
+	* @param index - индекс 
+	 */
+	void insert(const int val, const  int index);
+	/**
+	* @breaf удаляет элемент по значению
+	* @param val - значение элемента
+	 */
+	void remove(const int val);
+	/**
+	* @breaf вставляет элемент в конец очереди
+	* @param val - значение элемента
+	 */
+	void insert_back(const int val);
+	/**
+	* @breaf считывает элемент по значению
+	* @param index - индекс извлекаемого элемента
+	* @return значение элемента
+	 */
+	int get_val(const int index) const;
+	/**
+	* @breaf читает первый элемент
+	 */
+	void read_head() const;
+	/**
+	* @breaf ищет элемент по значению
+	* @param val - значение элемента
+	* @return индекс искомого элемента
+	 */
+	size_t search(const int val) const;
+	/**
+	* @breaf проверяет есть ли в очереди элементы
+	* @return true, если очередь пустая
+	* @return false, если в очереди есть хотя бы один элемент
+	 */
+	bool empty() const;
+	int get_size();
+	int get_front();
+	int get_back();
+};
